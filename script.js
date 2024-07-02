7f7d9b71802908617de712d234d8eb6e3a6b58d4
Slava.Gvsh <slava.gvsh@gmail.com>
2024-07-02 17:45:26 +0200

    add error

--- script.js
+++ script.js
@@ -1,22 +1,34 @@
 const button = document.querySelector(".todo__btn");
 const input = document.querySelector(".todo__input");
 const list = document.querySelector(".todo__list");
+const errorDiv = document.querySelector(".todo__error");
 // console.log(button);
 // console.log(input);
 // console.log(list);
 button.addEventListener("click", () => {
-  //   console.log(input.value);
-  const li = document.createElement("li");
-  li.className = "todo__item";
-  const deleteBtn = document.createElement("button");
-  li.innerText = input.value;
-  deleteBtn.innerText = "Delete";
-  list.appendChild(li);
-  li.appendChild(deleteBtn);
-  input.value = "";
+  const inputValue = input.value.trim();
 
-  deleteBtn.addEventListener("click", () => {
-    list.removeChild(li);
-  });
-  
+  if (inputValue === "") {
+    errorDiv.style.display = "block";
+    return;
+  }
+
+  errorDiv.style.display = "none";
+
+ 
+    //   console.log(input.value);
+    const li = document.createElement("li");
+    li.className = "todo__item";
+    const deleteBtn = document.createElement("button");
+    li.innerText = input.value;
+    deleteBtn.innerText = "Delete";
+    list.appendChild(li);
+    li.appendChild(deleteBtn);
+    input.value = "";
+
+    deleteBtn.addEventListener("click", () => {
+      list.removeChild(li);
+    });
+ 
 });
+
